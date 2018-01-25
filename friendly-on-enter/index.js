966322a015d952471dc22ca181a5d8941e6dfe23
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-01-26 00:08:34 +0100

    Refactor userFriendlyOnEnter, fix list behaviour
    Last empty list-item gets deleted on doubleEnter

--- friendly-on-enter/index.js
+++ friendly-on-enter/index.js
@@ -6,19 +6,27 @@ const isEnterHotKey = isKeyHotkey('enter');
 
 const userFriendlyOnEnterBehaviour = () => ({
     onKeyDown: (event, change) => {
-        if (
-            (isEnterHotKey(event) && change.value.startBlock.type !== 'list-item') ||
-            (isEnterHotKey(event) &&
-                change.value.startBlock.type === 'list-item' &&
-                change.value.startBlock.isEmpty)
-        ) {
+        if (isEnterHotKey(event)) {
             const currentSelection = change.value.selection;
             const endOfBlockSelection = change.extendToEndOf(change.value.startBlock).value
                 .selection;
-            if (isEqual(currentSelection, endOfBlockSelection)) {
+            if (
+                change.value.startBlock.type !== 'list-item' &&
+                isEqual(currentSelection, endOfBlockSelection)
+            ) {
                 event.preventDefault();
                 change.insertBlock(Block.create('paragraph'));
-                change.unwrapBlock('bulleted-list').unwrapBlock('numbered-list');
+                return false;
+            } else if (
+                change.value.startBlock.type === 'list-item' &&
+                change.value.startBlock.isEmpty &&
+                isEqual(currentSelection, endOfBlockSelection)
+            ) {
+                event.preventDefault();
+                change
+                    .setBlock('paragraph')
+                    .unwrapBlock('bulleted-list')
+                    .unwrapBlock('numbered-list');
                 return false;
             }
             change.select(currentSelection);
